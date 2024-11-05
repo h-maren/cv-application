ae92c59ace72386bea6ac38db7e82f52871c784f
h-maren <26232423+h-maren@users.noreply.github.com>
2024-11-05 08:47:20 -0500

    Add ability to add multiple work experiences

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,19 @@ function App() {
     phone: '',
   })
 
+  const [workData,setWorkData] = useState({
+    company: '',
+    startDate: new Date(),
+    endDate: new Date(),
+    position: '',
+    description: '',
+  })
+
+  const [workExperiences,setWorkExperiences] = useState([]);
+
   const [showResult, setShowResult] = useState(false);
 
+  let nextId=0;
   
   const handleSubmit = (e) => {
       e.preventDefault();
@@ -25,6 +36,23 @@ function App() {
     setFormData((prevFormData) => ({...prevFormData, [name]: value,
     }));
   };
+
+  const handleWorkChange = (e) =>{
+    const {name, value} = e.target;
+    setWorkData((prevWorkData) => ({...prevWorkData, [name]: value,
+    }));
+  };
+
+  const addWorkExperience = (e) => {
+    setWorkExperiences([...workExperiences, 
+      {
+      company: workData.company,
+      startDate: workData.startDate,
+      endDate: workData.endDate,
+      position: workData.position,
+      description: workData.description
+    }])
+  }
 
   const handleEditButton = (e) => {
       e.preventDefault();
@@ -40,13 +68,18 @@ function App() {
           {showResult && (
             <>
             <ContactInfo firstName={formData.firstName} lastName={formData.lastName} email={formData.email} phone={formData.phone} />
+            <ul>
+              {workExperiences.map(experience => (
+                <li key={experience.startDate}>{experience.company} {experience.position}</li>
+              )) }
+            </ul>
             <div className="edit-bar">
               <button type="button" onClick={handleEditButton}>Edit</button>
             </div>
             </>
           )}
       </div>
-      <div className="input-bar">
+      <section className="contact-input">
             {!showResult && (
             <form onSubmit={handleSubmit}>
               <div className="input-row">
@@ -76,7 +109,50 @@ function App() {
               <button type="submit">Generate Resume</button>
             </form>
             )}
-      </div>
+      </section>
+      <section className="work-input">
+            {!showResult && (
+              <>
+            <h3>Add Work Experience</h3>
+            <form>
+              <div className="input-row">
+              <label htmlFor="workCompany">
+                Company:
+              </label>
+              <input type="text" name="company" id="company" value = {workData.company} onChange={handleWorkChange} required />
+              </div>
+              <div className="input-row">
+              <label htmlFor="workPosition">
+                Position:
+              </label>
+              <input type="text" name="position" id="position" value = {workData.position} onChange={handleWorkChange} required />
+              </div>
+              <div className="input-row">
+                <label htmlFor="workStartDate">
+                  Start Date:
+                </label>
+                <input type="date" name="startDate" id="startDate" value = {workData.startDate} onChange={handleWorkChange} required />
+                <label htmlFor="workStartDate">
+                  End Date:
+                </label>
+                <input type="date" name="endDate" id="endDate" value = {workData.endDate} onChange={handleWorkChange} required />
+              </div>
+              <div className="input-row">
+              <label htmlFor="workDescription">
+                Description:
+              </label>
+              <input type="textarea" name="description" id="description" value = {workData.description} onChange={handleWorkChange} required />
+              </div>
+                <button type="button" onClick={addWorkExperience}>Add Work Experience</button>
+            </form>
+            <ul>
+              {workExperiences.map(experience => (
+                <li key={experience.startDate}>{experience.company} {experience.position}</li>
+              )) }
+            </ul>
+            </>
+            )}
+      </section>
     </>
   )
 }
